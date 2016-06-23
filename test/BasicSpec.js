723e4af4073277c75fc13421e4b79efbd1b52521
Kurien K. Joseph <kurien.joseph@bell.ca>
2016-06-23 10:15:58 -0400

    Expect true'

--- test/BasicSpec.js
+++ test/BasicSpec.js
@@ -2,7 +2,7 @@ define(['lang'], function(l) {
 
     describe("Basic Test Spec", function() {
         it("simple test", function() {
-            expect(true).toBe(false);
+            expect(true).toBe(true);
         });
         it("Language Get Test", function() {
             var obj = {'messageen': 'Hello', 'messagefr': 'Allo'};
