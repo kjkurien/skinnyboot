12a135e2a03686ea0012c540a3f776260df88682
Kurien K. Joseph <kurien.joseph@bell.ca>
2016-06-23 10:58:52 -0400

    Updated with ESLint

--- app/lang.js
+++ app/lang.js
@@ -1,20 +1,20 @@
 var LANG = (function () {
-    "use strict";
+    'use strict';
     var self = {};
-    self.LANG = "data-en";
-    self.ENGLISH = "data-en";
-    self.FRENCH = "data-fr";
+    self.LANG = 'data-en';
+    self.ENGLISH = 'data-en';
+    self.FRENCH = 'data-fr';
     self.get = function (obj) {
-		if (obj) {
-			if (self.LANG === self.ENGLISH) {
-            	return obj.messageen;
-        	} else if (self.LANG === self.FRENCH) {
-            	return obj.messagefr;	
-			}
-		}
+        if (obj) {
+            if (self.LANG === self.ENGLISH) {
+                return obj.messageen;
+            } else if (self.LANG === self.FRENCH) {
+                return obj.messagefr;	
+            }
+        }
         return '';
     };
-	self.getmsg = function (obj) {
+    self.getmsg = function (obj) {
         if (self.LANG === self.ENGLISH) {
             return obj.messageen;
         } else if (self.LANG === self.FRENCH) {
@@ -31,20 +31,20 @@ var LANG = (function () {
         }
     };
     self.viewTranslate = function (select) {
-        var selector = "[data-en]", ls = {}, i = 0;
+        var selector = '[data-en]', ls = {}, i = 0;
         if (select) {
-            selector = select + " " + selector;
+            selector = select + ' ' + selector;
         }
         ls = $(selector);
         for (i = 0; i < ls.length; i += 1) {
             $(ls[i]).text($(ls[i]).attr(self.LANG));
         }
 		
-		if (self.LANG === self.ENGLISH) {
-			$(".english").show();
-		} else {
-			$(".french").show();
-		}
+        if (self.LANG === self.ENGLISH) {
+            $('.english').show();
+        } else {
+            $('.french').show();
+        }
     };
     return self;
 }());
--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'app/*.js': ['eslint']
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
